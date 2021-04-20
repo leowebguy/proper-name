a6957d1c50647448d10dbfa498eaee2ed335f461
Leo <leowebguy@gmail.com>
2021-04-19 20:28:18 -0400

    add unit testing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Proper Name is a Craft 3 plugin intended to reduce liability and improve SEO by preventing biased (gender, ethnicity...) and/or copyrighted (shutterstock, getty...) assets naming and upload.
 
-![Screenshot](resources/proper1.png)
+![Screenshot](resources/proper.png)
 
 ---
 
@@ -15,3 +15,15 @@ composer require leowebguy/craft-proper-name
 ```
 
 In the Control Panel, go to Settings → Plugins → "Proper Name" → Install
+
+## Testing
+
+Follow [these](https://craftcms.com/docs/3.x/testing/testing-craft/setup.html) steps to setup Codeception then symlink the test file `vendor/leowebguy/craft-proper-name/tests/unit/ProperNameTest.php` into `tests/unit/` folder executing the command below from your project root.
+
+```bash
+ln -s ../../vendor/leowebguy/craft-proper-name/tests/unit/ProperNameTest.php ./tests/unit/ProperNameTest.php;
+```
+
+Then run `php vendor/bin/codecept run unit` from your root
+
+![Screenshot](resources/test.png)
---
+++ tests/unit/ProperNameTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Proper Name plugin for Craft CMS 3.x
+ * This plugin reduces liability and improves SEO by preventing biased (gender, ethnicity...),
+ * copyrighted (shutterstock, getty...) and other not desired/recommended assets naming.
+ *
+ * @author     Leo Leoncio
+ * @see        https://github.com/leowebguy
+ * @copyright  Copyright (c) 2021, leowebguy
+ * @license    MIT
+ */
+
+namespace leowebguy\propername\tests\unit;
+
+use Codeception\Test\Unit;
+//use Craft;
+//use craft\helpers\App;
+use leowebguy\propername\ProperName;
+use UnitTester;
+//use Codeception\Util\Debug;
+
+class ProperNameTest extends Unit
+{
+    /**
+     * @var UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+        parent::_before();
+    }
+
+    protected function _after()
+    {
+        parent::_after();
+    }
+
+    // Public methods
+    // =========================================================================
+    public function testProperNameInstance(): void
+    {
+        self::assertInstanceOf(ProperName::class, ProperName::$plugin);
+    }
+
+    public function testProperNameServiceMatchName(): void
+    {
+        $result = ProperName::getInstance()->propernameService->matchName('asset-shutterstock-047682.jpg');
+        self::assertArrayHasKey(0, $result); // match shutterstock
+    }
+
+    public function testProperNameGetSettings(): void
+    {
+        $result = ProperName::getInstance()->getSettings();
+        self::assertArrayHasKey('wordList', $result); // has wordList key
+        self::assertArrayHasKey('cacheTime', $result); // has cacheTime key
+    }
+
+    public function testProperNameWordListHasData(): void
+    {
+        $result = ProperName::getInstance()->getSettings()['wordList'];
+        self::assertIsArray($result); // wordList is array
+        self::assertArrayHasKey(0, $result); // has at least one entry
+    }
+
+//    public function testSiteGetStatusCode()
+//    {
+//        $client = Craft::createGuzzleClient();
+//        $response = $client->request('GET', 'http://' . App::env('SITE_URL') . '/');
+//        self::assertEquals($response->getStatusCode(), '200');
+//    }
+}
